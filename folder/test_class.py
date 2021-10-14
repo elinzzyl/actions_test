bf42b608a94fd0d72ff4097b01ef247036a73317
elin <elin@genegeniedx.com>
2021-10-14 10:36:18 +0800

    add yml and test files

--- folder/test_class.py
+++ folder/test_class.py
@@ -16,3 +16,7 @@ class TestClassTwo(object):
     def test_two(self):
         x = "apple"
         assert 'b' not in x
+
+if __name__ == '__main__':
+    # verbosity=*：默认是1；设为0，则不输出每一个用例的执行结果；2-输出详细的执行结果
+    unittest.main(verbosity=1)        
